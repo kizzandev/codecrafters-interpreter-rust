e21aa22c56ea0d379dde3ca4d4ab1b0cdbea7346
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-05 19:52:43 -0300

    pass 1st stage

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -107,7 +107,7 @@ pub fn tokenize(filename: &String) -> anyhow::Result<()> {
                     if c == '.' && !has_dot || c.is_ascii_digit() {
                         if c == '.' {
                             let mut peekable = chars.clone().peekable();
-                            if !peekable.next().is_ascii_digit() {
+                            if !Some(peekable.next()).is_ascii_digit() {
                                 break;
                             }
                             has_dot = true;
