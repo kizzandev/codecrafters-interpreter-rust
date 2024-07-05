8f36caac072819e35d003c95d22b744619fa0307
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-05 18:18:20 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ impl Token {
 
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        eprintln!(f, "{:?} {} {}", self._type, self._string, self._value.clone().unwrap_or("null".to_string()))
+        eprintln!("{:?} {} {}", self._type, self._string, self._value.clone().unwrap_or("null".to_string()));
     }
 }
 
