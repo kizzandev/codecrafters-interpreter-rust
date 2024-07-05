c18d86d5518c2cedf9feb47f9190e9a2a4f9d50b
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-05 20:06:19 -0300

    pass 1st stage

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -118,7 +118,11 @@ pub fn tokenize(filename: &String) -> anyhow::Result<()> {
                         }
                     }
                 }
-                tokens.push(Token::new_with_value(TokenType::NUMBER, number.clone(), number.to_string()));
+                let value if number.chars().last() == Some('.') {
+                    number.push('0');
+                    number[0..number.len()-1].to_string()
+                }
+                tokens.push(Token::new_with_value(TokenType::NUMBER, value, number.to_string()));
             }
             _ => {
                 eprintln!("[line {}] Error: Unexpected character: {}", line, c);
