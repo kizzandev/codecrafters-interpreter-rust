66ae5d9f4b74e964f0a0ef70748df11c2a8070bc
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-05 18:02:12 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,56 @@
 use std::env;
 use std::fs;
-use std::io::{self, Write};
+use std::fmt::Display;
+
+enum Token {
+    LeftParen,
+    RightParen,
+
+    Unknown,
+    Eof,
+    Total_tokens,
+}
+
+impl From<char> for Token {
+    fn from(c: char) -> Self {
+        match c {
+            '(' => Token::LeftParen,
+            ')' => Token::RightParen,
+            _ => Token::Unknown,
+        }
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Token::LeftParen => "LEFT_PAREN",
+            Token::RightParen => "RIGHT_PAREN",
+            Token::Unknown => "UNKNOWN",
+            Token::Eof => "EOF",
+        };
+        f.write_str(s)
+    }
+}
+
+struct TokenResult(Token, String);
+
+impl From<char> for TokenResult {
+    fn from(c: char) -> Self {
+        Self(Token::from(c), c.to_string())
+    }
+}
+
+impl Display for TokenResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("{} {}", self.0, self.1))
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
+        eprintln!("Usage: {} tokenize <filename>", args[0]);
         return;
     }
 
@@ -14,23 +59,26 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            // writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+                eprintln!("Failed to read file {}", filename);
                 String::new()
             });
 
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                for char in file_contents.chars() {
+                    if char == '\n' {
+                        continue;
+                    }
+                    let result = TokenResult::from(char);
+                    println!("{}", result);
+                }
+                println!("EOF  null");
             } else {
                 println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             }
         }
         _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
+            eprintln!("Unknown command: {}", command);
         }
     }
 }
