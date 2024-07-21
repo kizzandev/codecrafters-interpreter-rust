2bd6a38bb6f23645edc429642508272a0455a64a
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-21 16:41:26 -0300

    pass stage

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use std::{io, fs};
+
 pub fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
 
-pub fn read_file(filename) -> String {
+pub fn read_file(filename: &str) -> String {
     fs::read_to_string(filename).unwrap_or_else(|err| {
         writeln!(io::stderr(), "Problem opening the file: {}", err).unwrap();
         String::new()
