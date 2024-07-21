82c314a86a43866fc5bff1c788ffb9dc1785abb9
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-21 20:49:14 -0300

    pass stage

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -13,7 +13,7 @@ pub fn tokenize(file_contents: &str) -> ExitCode {
             Token::StringLiteral(s) => println!("STRING \"{s}\" {s}"),
             Token::UnterminatedStringLiteral => {
                 success = false;
-                println!("[line {line}] Error: Unterminated string.");
+                eprintln!("[line {line}] Error: Unterminated string.");
             },
             Token::Number((raw_s, n)) => println!("NUMBER {raw_s} {n:?}"),
             Token::CharacterDouble(c1, c2) => {
