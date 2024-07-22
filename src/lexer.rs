4684ca58f4b64c5a8d9bea4d93710fb895c5e633
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-21 21:48:34 -0300

    pass stage

--- src/lexer.rs
+++ src/lexer.rs
@@ -155,12 +155,6 @@ impl<'input> Iterator for Lexer<'input> {
                         continue;
                     }
                 };
-
-                let n_raw = if !has_dot {
-                    Box::leak(format!("{n_raw}.0").into_boxed_str())
-                } else {
-                    n_raw
-                };
                 return Some((Token::Number((n_raw, n)), self.line));
             }
         }
