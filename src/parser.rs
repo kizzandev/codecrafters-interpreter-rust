53908374e373e954dc3b6e330a59e0b54412bd77
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-22 13:25:10 -0300

    pass stage

--- src/parser.rs
+++ src/parser.rs
@@ -83,7 +83,7 @@ fn recursive_parse(lexer: &mut Lexer, depth: usize) -> Result<String, ExitCode> 
                 loop {
                     let right = recursive_parse(lexer, depth)?;
                     left = format!("({op} {left} {right})");
-                    if let Some((Toke::Character(next_op), _)) = lexer.peek() {
+                    if let Some((Token::Character(next_op), _)) = lexer.peek() {
                         if matches!(next_op, '*' | '/') {
                             let _ = lexer.next();
                             continue;
