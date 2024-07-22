79654e02be56be9a8c9d72dbe35c868d3483a8cf
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-21 23:56:05 -0300

    pass stage

--- src/parser.rs
+++ src/parser.rs
@@ -78,6 +78,7 @@ fn recursive_parse(lexer: &mut Lexer, depth: usize) -> Result<String, ExitCode> 
     if depth > 0 {
         eprintln!("Depth: {depth}");
         eprintln!("Error: Unmatched parentheses.");
+        eprintln!("Contents: {result}");
         return Err(ExitCode::from(65));
     }
     Ok(result)
