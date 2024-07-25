d9c8f43ecd334e0642832cda85c7b0ca740dc378
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-24 23:45:58 -0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use std::process::ExitCode;
 mod tokenizer;
 mod parser;
 mod lexer;
-mod loclib;
 
 use crate::tokenizer::tokenize;
 use crate::parser::parse;
-use crate::loclib::read_file;
+use interpreter_starter_rust::read_file;
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
