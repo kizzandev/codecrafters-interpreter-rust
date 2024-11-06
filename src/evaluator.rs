799db49ec9e050f43db9aabf5fe44d60e0f8d36e
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-11-05 22:22:28 -0300

    check

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -87,7 +87,7 @@ impl Interpreter {
 
                 // eprintln!("EXPRS ARE: {:?} AND {:?}", left_literal, right_literal);
 
-                if !left_literal.is_same_type_weird(&right_literal) {
+                if left_literal.is_same_type_weird(&right_literal) {
                     return Ok(LiteralExpr::FALSE);
                 }
 
--- src/parser.rs
+++ src/parser.rs
@@ -38,16 +38,11 @@ impl LiteralExpr {
     }
 
     pub fn is_same_type_weird(&self, other: &LiteralExpr) -> bool {
-        // let first;
-        // let second;
         if matches!(
             (self, other),
             (LiteralExpr::Number(_), LiteralExpr::StringLiteral(_))
                 | (LiteralExpr::StringLiteral(_), LiteralExpr::Number(_))
         ) {
-            // first = self.to_string();
-            // second = other.to_string();
-            // return first == second;
             return true;
         }
         false
